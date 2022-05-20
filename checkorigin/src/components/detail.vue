55d4262f5b88f69dfe96614279ee6b137eda4842
Triet Dang <triet.dang@trecs.ai>
2022-05-20 14:01:26 +0700

    fix ui

--- checkorigin/src/components/detail.vue
+++ checkorigin/src/components/detail.vue
@@ -11,7 +11,29 @@
         </div>
         <div class="mainDetail row">
             <div class="row">
-                <div class="header-detail">
+                <div class="header-detail row">
+                    <div class="info-rec col-3">
+                        <p>Project Name</p>
+                        <b>T-RECSolarINSSRE1</b>
+                    </div>
+                    <span class="line"></span>
+                    <div class="info-rec col-2">
+                        <p>Asset ID</p>
+                        <b>GEN1234</b>
+                        
+                    </div>
+                    <span class="line"></span>
+                    <div class="info-rec col-2">
+                        <p>Asset Type</p>
+                        <b>Solar Energy REC</b>
+                        <span class="line"></span>
+                    </div>
+                    <span class="line"></span>
+                    <div class="info-rec col-4">
+                        <p>Loacation (2.292938, 102.342703)</p>
+                        <b>Rajasthan, India</b>
+                    </div>
+
                 </div>
             </div>
             <div class="row">
@@ -41,6 +63,16 @@
                 </div>
                 <div class="col-4">
                     <div class="rightDetail">
+                        <div class="description row">
+                            <b>Asset Description:</b>
+                            <p>GKSB Solar PV Farm is located in Melaka, Malaysia. It consists of Yingli Solar, Hanwha Q Cells and JA Solars panels. The solar farm is using EDMI Mk6E for metering. EDMI MK6E is a high-precision meter created for generation and transmission applications, as well as for revenue metering at high-end consumer facilities. The power plant is connected to grid.</p>
+                        </div>
+                        <div class="certification row">
+                            <img src="../assets/images/tigrs.png" class="img-certification" alt="">
+                            <p>RECs Certification   </p>
+                            <p>
+The trusted RECs are registered at The Tradable Instrument for Global Renewables (TIGR), a global renewable energy registry developed and managed by APX.</p>
+                        </div>
                         
                     </div>
                 </div>
@@ -65,11 +97,14 @@ p{
     position: relative;
     flex-direction: row-reverse;
 }
+.header-detail p{
+    margin-bottom: 10px;
+}
 .input-rec-detail{
     position: absolute;
-    height: 50px;
+    height: 40px;
     width: 320px;
-    margin: 25px 65px 25px 0px;
+    margin: 50px 65px 25px 0px;
     border-radius: 50px;
     background: #FFFFFF;
     mix-blend-mode: normal;
@@ -80,7 +115,7 @@ p{
     padding-left: 15px;
 }
 .logo-detail{
-    height: 80px;
+    height: 60px;
     width: 250px;
     margin: 25px 0px 25px 65px;
     border-radius: 0px;
@@ -91,7 +126,7 @@ p{
     position: absolute;
     width: 50px;
     height: 30px;
-    top: 35px;
+    top: 55px;
     right: 70px;
     background-repeat: no-repeat;
     border:0px;
@@ -99,13 +134,20 @@ p{
     margin-right: 10px;
     background-image: url('../assets/images/Shape.png');
 }
+.line{
+    height:100px;
+    width: 1px;
+    background-image: url('../assets/images/Line.png');
+    background-repeat: no-repeat;
+    background-position: center;
+}
 .btn-search-detail:hover{
     background-size: 20px;
     cursor: pointer;
 }
 .header-detail{
-    width: 57%;
-    height: 124px;
+    width: 80%;
+    height: 100px;
     margin: 25px 0px 10px 65px;
     background: #FFFFFF;
     border: 1px solid #E8E8E8;
@@ -124,14 +166,7 @@ p{
     margin-left:50px;
 }
 
-.rightDetail{
-    height: 600px;
-    background: #FFFFFF;
-    border: 1px solid #E8E8E8;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
-    border-radius: 20px;
-    margin-left:50px;
-}
+
 .img1{
     width: 100%;
     height: 300px;
@@ -151,5 +186,30 @@ p{
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
     border-radius: 20px;
     margin-left: 50px;
+}
+
+.description{
+    height: 300px;
+    margin-bottom: 10px;
+    background: #FFFFFF;
+    border: 1px solid #E8E8E8;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
+    border-radius: 20px;
+    margin-left: 50px;
+}
+.certification{
+    margin-bottom: 10px;
+    height: 300px;
+    background: #FFFFFF;
+    border: 1px solid #E8E8E8;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
+    border-radius: 20px;
+    margin-left: 50px;
+}
+
+.img-certification{
+    width: 80%;
+    height: 40%;
+    object-fit: contain;
 }
 </style>
